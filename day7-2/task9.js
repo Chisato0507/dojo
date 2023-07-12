efd357f0ae8faae283857365a2e245cfe97a5703
chisato0507 <t002690@cybozu.onmicrosoft.com>
2023-07-12 09:12:19 +0900

    re-re-re-kadai9

--- day7-2/task9.js
+++ day7-2/task9.js
@@ -6,10 +6,16 @@
   });
   kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], async (event) => {
     const Ban = event.record.重複禁止項目.value;
+    let query;
+    if (event.type === 'app.record.create.submit'){
+      query = `重複禁止項目 = "${Ban}"`;
+    } else {
+      const id = kintone.app.record.getId();
+      query = `$id != "${id}" and 重複禁止項目 = "${Ban}"`;
+    }
     const params = {
       'app': kintone.app.getId(),
-      'fields': '重複禁止項目',
-      'query': `重複禁止項目 = "${Ban}"`
+      'query': query
     };
     const resp = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params);
     const Banfield = resp.records;
